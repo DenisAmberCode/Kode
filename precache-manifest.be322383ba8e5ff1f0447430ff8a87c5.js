ad52ae7606c40d300e7ac0de27038fb6292c0b6e
DenisAmberCode <gladkiy-denis@mail.ru>
2020-10-27 23:17:15 +0200

    Updates

---
+++ precache-manifest.be322383ba8e5ff1f0447430ff8a87c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d02d63db8e54873202b7b7b2c886548",
+    "url": "/Kode/index.html"
+  },
+  {
+    "revision": "93dcdc1dffa88b26ae6c",
+    "url": "/Kode/static/css/main.bd52e306.chunk.css"
+  },
+  {
+    "revision": "50c78b895c2b08bff258",
+    "url": "/Kode/static/js/2.07da6197.chunk.js"
+  },
+  {
+    "revision": "40ff5562bfbe0dc926d145d5290e83c2",
+    "url": "/Kode/static/js/2.07da6197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dcdc1dffa88b26ae6c",
+    "url": "/Kode/static/js/main.aeb4e3cd.chunk.js"
+  },
+  {
+    "revision": "efab41c215e7867326ee",
+    "url": "/Kode/static/js/runtime-main.3b395193.js"
+  }
+]);
