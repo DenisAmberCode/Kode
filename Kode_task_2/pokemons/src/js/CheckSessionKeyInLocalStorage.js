396262d1d8aba40750e4d7d0114843899d07fda8
DenisAmberCode <61059644+DenisAmberCode@users.noreply.github.com>
2020-10-28 14:11:19 +0200

    Update CheckSessionKeyInLocalStorage.js

--- Kode_task_2/pokemons/src/js/CheckSessionKeyInLocalStorage.js
+++ Kode_task_2/pokemons/src/js/CheckSessionKeyInLocalStorage.js
@@ -1,4 +1,4 @@
-// Заглушка: в идеале sessionKeyValid приходит с сервера
+// Заглушка: в идеале проверка происходит на сервере
 export default function checkSessionKeyInLocalStorage() {
     const sessionKeyValid = 'ca2c6c862d4058c636cbf41bcacff0d8'; // hash: MD5(login + password).toString();
     const sessionKey = localStorage.getItem('sessionKey');
